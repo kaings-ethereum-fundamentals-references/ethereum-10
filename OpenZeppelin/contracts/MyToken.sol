0e5f7cf0bf99e4b674c6f5ff2bd115b80af7a1f8
willys_tjiawi <willys@huubap.com>
2021-10-25 00:29:34 +0800

    create ERC20 token using openzeppelin: mint token

--- OpenZeppelin/contracts/MyToken.sol
+++ OpenZeppelin/contracts/MyToken.sol
@@ -4,6 +4,6 @@ import '..//node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract MyToken is ERC20 {
     constructor() ERC20("mytoken", "MTK") public {
-
+        _mint(msg.sender, 1000);
     }
 }
