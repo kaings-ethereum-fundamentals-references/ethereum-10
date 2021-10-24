a77eac7bf7915b84c05422a6167827159ab12c20
willys_tjiawi <willys@huubap.com>
2021-10-25 00:39:40 +0800

    create ERC20Capped token using openzeppelin

--- OpenZeppelin/contracts/MyToken.sol
+++ OpenZeppelin/contracts/MyToken.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.8.0;
 
-import '..//node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '..//node_modules/@openzeppelin/contracts/token/ERC20/Extensions/ERC20Capped.sol';
 
-contract MyToken is ERC20 {
-    constructor() ERC20("mytoken", "MTK") public {
-        _mint(msg.sender, 1000);
+contract MyToken is ERC20Capped {
+    constructor() ERC20("mytoken", "MTK") ERC20Capped(100000) public {      // token is capped at 100K, exceeding the limit will throw an error
+        _mint(msg.sender, 1000);        // e.g. refactoring to `_mint(msg.sender, 200000)`, it will throw an error 
     }
 }
