6f9611ad9c796f1814c62808ba567fd9965ec935
willys_tjiawi <willys@huubap.com>
2021-10-24 23:52:02 +0800

    create ERC20 token using openzeppelin

---
+++ OpenZeppelin/contracts/MyToken.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.0;
+
+import '..//node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract MyToken is ERC20 {
+    constructor() ERC20("mytoken", "MTK") public {
+
+    }
+}
